e18950af59ea6bfce98c0a676abecdafa968f7ed
Johann <git@johann-hofmann.com>
2014-09-19 00:41:09 +0200

    read input file

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,30 @@
 extern crate regex_macros;
 extern crate regex;
 
+use std::io::{File, Open, ReadWrite};
+
 mod parser;
 mod html;
+mod normalizer;
+
 
 fn main() {
-    let text = " #### Hello World
-                    ## Also
-                    Stuff
-                    ";
-    let result = parser::parse(text);
+    let path = Path::new("test.md");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => fail!("couldn't open {}: {}", display, why.desc),
+        Ok(file) => file,
+    };
+
+    let text = match file.read_to_string() {
+        Err(why) => fail!("couldn't read {}: {}", display, why.desc),
+        Ok(string) => string
+    };
+
+    let normalized = normalizer::normalize(text.as_slice());
+    println!("{}", normalized);
+    let result = parser::parse(normalized.as_slice());
     println!("{}", html::to_html(result));
 }
 
