95fc737f048fd5a596993c6f264e133697a88863
Johann <git@johann-hofmann.com>
2014-09-19 00:40:35 +0200

    add linebreak to html output

--- src/html.rs
+++ src/html.rs
@@ -1,13 +1,14 @@
 use parser;
+use parser::Element;
 use parser::Header;
 use parser::Paragraph;
 
-pub fn to_html (tokens : Vec<parser::Token>) -> String {
+pub fn to_html (tokens : Vec<Element>) -> String {
     let mut ret = String::new();
     for token in tokens.iter(){
-        let next = match token.el {
-            Header (text, level) => format!("<h{}>{}</h{}>", level, text, level),
-            Paragraph (text) => format!("<p>{}</p>", text)
+        let next = match token {
+            &Header (text, level) => format!("<h{}>{}</h{}>\n", level, text, level),
+            &Paragraph (text) => format!("<p>{}</p>\n", text)
         };
         ret.push_str(next.as_slice())
     }
