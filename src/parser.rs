50efc650990c4075c75c0b31433e18fb6ca05dd7
Johann <git@johann-hofmann.com>
2014-09-19 00:41:28 +0200

    add setext headers

--- src/parser.rs
+++ src/parser.rs
@@ -1,48 +1,46 @@
 use regex::Regex;
 
 #[deriving(Show)]
-pub struct Token {
-    pub el : Element,
-    pub md : &'static str,
+pub enum Element<'s> {
+    Header(&'s str, uint),
+    Paragraph(&'s str)
 }
 
-#[deriving(Show)]
-pub enum Element {
-    Header(&'static str, uint),
-    Paragraph(&'static str)
-}
+static SPLIT : Regex = regex!(r"\n\n");
+static ATX_HEADER : Regex = regex!(r"(?P<level>#{1,6})\s(?P<text>.*)");
+static SETEXT_HEADER_1 : Regex = regex!(r"(?P<text>.+)\n===+");
+static SETEXT_HEADER_2 : Regex = regex!(r"(?P<text>.+)\n---+");
 
-static SPLIT : Regex = regex!(r"\n");
-static HEADER : Regex = regex!(r"\s*(?P<level>#{1,6})(?P<text>.*)");
-
-//fn is_header(&iter : Iterator<String>) -> bool{
-
-//}
-
-pub fn parse (md : &'static str) -> Vec<Token> {
+pub fn parse (md : &str) -> Vec<Element> {
     let mut split = SPLIT.split(md).peekable();
     let mut tokens = vec![];
     for text in split{
-      tokens.push(parse_segment(text));
+        tokens.push(parse_segment(text));
     }
     tokens
 }
 
-fn parse_segment(text : &'static str) -> Token{
-    if HEADER.is_match(text){
-        let caps = HEADER.captures(text).unwrap();
-        return Token{
-            el : Header
-            (
-                caps.name("text"),
-                caps.name("level").len()
-            ),
-            md: text
-        };
+fn parse_segment (text : &str) -> Element{
+    if ATX_HEADER.is_match(text){
+        let caps = ATX_HEADER.captures(text).unwrap();
+        return Header (
+            caps.name("text"),
+            caps.name("level").len()
+            );
+    }else if SETEXT_HEADER_1.is_match(text){
+        let caps = SETEXT_HEADER_1.captures(text).unwrap();
+        return Header (
+            caps.name("text"),
+            1
+            );
+    }else if SETEXT_HEADER_2.is_match(text){
+        let caps = SETEXT_HEADER_2.captures(text).unwrap();
+        return Header (
+            caps.name("text"),
+            2
+            );
     }else{
-      return Token{
-          el : Paragraph(text),
-          md: text
-      };
+        return Paragraph(text);
     }
 }
+
