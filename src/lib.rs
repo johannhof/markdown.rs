00bb3053825dc20c49d56e82ac3bf72050a9ad26
Johann <git@johann-hofmann.com>
2014-12-01 23:40:12 +0100

    Add file_to_html, avoid duplication in main

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@
 extern crate regex_macros;
 extern crate regex;
 
+use std::io::File;
+use std::io::IoError;
+
 mod parser;
 mod html;
 mod normalizer;
@@ -13,5 +16,21 @@ pub fn to_html(text : &str) -> String{
     let normalized = normalizer::normalize(text.as_slice());
     let result = parser::parse(normalized.as_slice());
     html::to_html(&result)
+}
+
+pub fn file_to_html(path : &Path) -> Result<String, IoError>{
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
+
+    let text = match file.read_to_string() {
+        Ok(string) => string,
+        Err(e) => return Err(e)
+    };
+
+    let normalized = normalizer::normalize(text.as_slice());
+    let result = parser::parse(normalized.as_slice());
+    Ok(html::to_html(&result))
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,9 @@
-#![feature(phase)]
-#[phase(plugin)]
-extern crate regex_macros;
-extern crate regex;
-
-use std::io::File;
-
-mod parser;
-mod html;
-mod normalizer;
+extern crate markdown;
 
 fn main() {
     let path = Path::new("test.md");
     let display = path.display();
 
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why.desc),
-        Ok(file) => file,
-    };
-
-    let text = match file.read_to_string() {
-        Err(why) => panic!("couldn't read {}: {}", display, why.desc),
-        Ok(string) => string
-    };
-
-    let normalized = normalizer::normalize(text.as_slice());
-    println!("{}", normalized);
-    let result = parser::parse(normalized.as_slice());
-    println!("{}", html::to_html(&result));
+    println!("{}", markdown::file_to_html(&path));
 }
 
