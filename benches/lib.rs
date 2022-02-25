3ca7faa751ad008f1bbc4403877c57fcd0af5c8c
Phlosioneer <mattmdrr2@gmail.com>
2022-02-25 03:50:35 -0500

    Add large benchmark
    Add large benchmark to check for quadratic algorithms.

--- benches/lib.rs
+++ benches/lib.rs
@@ -7,7 +7,6 @@ extern crate test;
 use test::Bencher;
 use std::fs;
 
-// Current benchmark: 3,055,292 ns/iter (+/- 328,418)
 #[bench]
 fn parse_markdown_file(b: &mut Bencher) {
     let md = fs::read_to_string("benches/files/Markdown.md").unwrap();
@@ -16,4 +15,26 @@ fn parse_markdown_file(b: &mut Bencher) {
     test::black_box(markdown::tokenize(&md));
     
     b.iter(|| markdown::tokenize(&md));
+}
+
+/// This is a sanity check. It should be roughly 100x the run time of parse_markdown_file,
+/// UNLESS some n^2 algorithm snuck into the parser. Other than that, it's not very useful;
+/// it's no more accurate or precise than parse_markdown_file.
+/// 
+/// Due to the large number of allocations involved, it should be slighly more expensive since
+/// we're applying more stress to the allocator. In practice we should expect roughly 110x the
+/// run time of parse_markdown_file.
+#[bench]
+fn parse_100_markdown_files_concatenated(b: &mut Bencher) {
+    let md = fs::read_to_string("benches/files/Markdown.md").unwrap();
+    let mut concat = String::with_capacity((md.len() + 1) * 100);
+    for _ in 0..100 {
+        concat.push_str(&md);
+        concat.push('\n');
+    }
+
+    // Force all the lazy_static's to initialize
+    test::black_box(markdown::tokenize(&md));
+    
+    b.iter(|| markdown::tokenize(&concat));
 }
